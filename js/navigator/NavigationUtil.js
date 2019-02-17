50b892d45e573e7b44475ce400f278c1ae7db124
chenping <chenping@wecash.net>
2019-02-17 21:31:01 +0800

    add NavigationUtil

---
+++ js/navigator/NavigationUtil.js
@@ -0,0 +1,21 @@
+/**
+ * 全局导航控制工具类
+ * */
+
+export default class NavigationUtil {
+  /**
+  * 返回上一页
+   * @param navigation
+  * */
+  static resetToHomePage(navigation){
+    navigation.goBack();
+  }
+  /**
+   * 重置到首页
+   * @param navigation
+   * */
+  static resetToHomePage(params){
+    const {navigation} = params;
+    navigation.navigate("Main");
+  }
+}
--- js/page/WelcomePage.js
+++ js/page/WelcomePage.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import NavigationUtil from "../navigator/NavigationUtil";
 
 type Props = {};
 export default class WelcomePage extends Component<Props> {
+  // 设置定时器，2秒之后从欢迎页跳转到首页
+  componentDidMount(){
+    this.timer = setTimeout(() => {
+      NavigationUtil.resetToHomePage({
+        navigation: this.props.navigation
+      })
+    },2000);
+  }
+
+  componentWillUnmount(){
+    this.timer && clearTimeout(this.timer);
+  }
+
   render() {
     return (
       <View style={styles.container}>
