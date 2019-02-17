7f569bdf5c27e435201c0bc2bf95e56cc8242dd9
chenping <chenping@wecash.net>
2019-02-17 20:21:02 +0800

    add DetailPage and fix navigators

--- index.js
+++ index.js
@@ -4,12 +4,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import AppNavigator from './js/navigator/AppNavigators';
 import {name as appName} from './app.json';
 
-import WelcomePage from './js/page/WelcomePage';
-import HomePage from './js/page/HomePage';
-
-
 // 整个app的入口，链接到指定的js页面
-AppRegistry.registerComponent(appName, () => WelcomePage);
+AppRegistry.registerComponent(appName, () => AppNavigator);
