604b87a332c0b2bbd153cc3813c89f626506a102
chenping <chenping@wecash.net>
2019-02-17 19:55:07 +0800

    add navigation

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import HomePage from "./js/page/HomePage";
+import WelcomePage from "./js/page/WelcomePage";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
--- index.js
+++ index.js
@@ -7,5 +7,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-// 整个app的入口，链接到App.js
-AppRegistry.registerComponent(appName, () => App);
+import WelcomePage from './js/page/WelcomePage';
+import HomePage from './js/page/HomePage';
+
+
+// 整个app的入口，链接到指定的js页面
+AppRegistry.registerComponent(appName, () => WelcomePage);
