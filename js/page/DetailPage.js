a7ecef6c8fce4a5ebe1ce7fc84e4d2bcff1bd91a
chenping <chenping@wecash.net>
2019-02-22 14:19:00 +0800

    change detailPage

--- js/page/DetailPage.js
+++ js/page/DetailPage.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import NavigationBar from "../common/NavigationBar";
+import ViewUtil from "../util/ViewUtil";
 
+cosnt
 type Props = {};
 export default class DetailPage extends Component<Props> {
+  constructor(props) {
+    super(props);
+    //从导航器中将params取出
+    this.params = this.props.navigation.state.params;
+    const {projectModel} = this.params;
+    this.url = projectModel.html_url || projectModel.fullname;
+    this.state = {
+      title:
+    }
+  }
+  onBack() {
+
+  }
+
   render() {
+    let navigationBar = <NavigationBar
+      title={"旅游信息"}
+      leftButton={ViewUtil.getLeftBackButton(() => this.onBack())}
+      statusBar={statusBar}
+      style={{backgroundColor: TITLE_COLOR}}
+    />
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>DetailPage</Text>
+        {navigationBar}
       </View>
     );
   }
---
+++ js/util/ViewUtil.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {TouchableOpacity, } from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+export default class ViewUtil {
+
+  /**
+   * 获取左侧返回按钮
+   * @param callBack
+   * @returns {XML}
+   * **/
+  static getLeftBackButton(callBack) {
+    return <TouchableOpacity
+      style={{padding: 8, paddingLeft: 12}}
+      onPress={callBack}
+    >
+      <Ionicons
+        name={'ios-arrow-back'}
+        size={26}
+        style={{color: "white"}}
+      />
+    </TouchableOpacity>
+  }
+}
