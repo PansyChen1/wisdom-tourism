254ee29943a936679443a5aade45bc41607c038c
chenping <chenping@wecash.net>
2019-03-26 16:14:01 +0800

    change FirstPage

--- js/page/FirstPage.js
+++ js/page/FirstPage.js
@@ -15,12 +15,30 @@ export default class FirstPage extends Component {
   getLocation(){
     Geolocation.reverseGeoCodeGPS(116.58, 35.42)
       .then(data => {
-        alert('reverseGeoCodeGPS:', data);
+        alert('reverseGeoCodeGPS:' + data);
       })
       .catch(e => {
         console.log(e, "error");
       })
   }
+
+  // getLocation(){
+  //   Geolocation.getCurrentPosition(
+  //     location => {
+  //       var result = "\n经度：" + location.coords.longitude +
+  //         "\n纬度：" + location.coords.latitude +
+  //         "\n准确度：" + location.coords.accuracy +
+  //         "\n行进方向：" + location.coords.heading +
+  //         "\n海拔：" + location.coords.altitude +
+  //         "\n海拔准确度：" + location.coords.altitudeAccuracy +
+  //         "\n时间戳：" + location.timestamp;
+  //       alert(result);
+  //     },
+  //     error => {
+  //       alert("获取位置失败："+ error)
+  //     }
+  //   );
+  // }
 
   renderLeftButton() {
     return <TouchableOpacity
