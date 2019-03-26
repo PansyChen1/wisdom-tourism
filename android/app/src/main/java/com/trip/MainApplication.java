9db6a74aa1a0219023a4a658e6a008c9c9662382
chenping <chenping@wecash.net>
2019-03-26 09:01:28 +0800

    add baiduMap

--- android/app/src/main/java/com/trip/MainApplication.java
+++ android/app/src/main/java/com/trip/MainApplication.java
@@ -3,6 +3,7 @@ package com.trip;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.lovebing.reactnative.baidumap.BaiduMapPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BaiduMapPackage(),
             new RNCWebViewPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage(),
